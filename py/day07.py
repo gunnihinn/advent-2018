3a84860950cd47943e6bcab438b3cd2b60ce1730
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2018-12-07 11:55:33 +0100

    Solve day 7, part A

--- py/day07.py
+++ py/day07.py
@@ -1,6 +1,71 @@
+import collections
+
+
 def run_a(filename):
-    pass
+    with open(filename) as fh:
+        lines = fh.readlines()
+        graph = build_graph(parse(lines))
+        lin = linearize(graph)
+        print(''.join(lin))
 
 
 def run_b(filename):
     pass
+
+
+def parse(lines):
+    pairs = []
+    for line in lines:
+        parts = line.split()
+        pairs.append((parts[1], parts[7]))
+
+    return pairs
+
+
+def build_graph(pairs):
+    graph = collections.defaultdict(list)
+    for (a, b) in pairs:
+        graph[a].append(b)
+
+    for v in graph.values():
+        v.sort()
+
+    return graph
+
+
+def alphabet(graph):
+    alpha = set()
+    for k, vs in graph.items():
+        alpha.add(k)
+        alpha.update(vs)
+
+    return alpha
+
+
+def find_no_deps(graph):
+    alpha = alphabet(graph)
+    for deps in graph.values():
+        alpha.difference_update(deps)
+
+    return alpha
+
+
+def linearize(graph):
+    available = find_no_deps(graph)
+    lin = []
+    deps = None
+
+    while available and graph:
+        av = sorted(list(available))
+        n = av[0]
+
+        lin.append(n)
+        available.remove(n)
+        deps = graph.pop(n)
+
+        available.update(find_no_deps(graph))
+
+    if deps:
+        lin += sorted(deps)
+
+    return lin
--- py/tests/day07_test.py
+++ py/tests/day07_test.py
@@ -4,5 +4,70 @@ from py.day07 import *
 
 class TestExample(unittest.TestCase):
 
-    def test_nothing(self):
-        pass
+    def test_parse(self):
+        lines = """
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+""".strip().split('\n')
+        self.assertEqual(parse(lines), [
+            ('C', 'A'),
+            ('C', 'F'),
+            ('A', 'B'),
+            ('A', 'D'),
+            ('B', 'E'),
+            ('D', 'E'),
+            ('F', 'E'),
+        ])
+
+    def test_build_graph(self):
+        pairs = [
+            ('C', 'A'),
+            ('C', 'F'),
+            ('A', 'B'),
+            ('A', 'D'),
+            ('B', 'E'),
+            ('D', 'E'),
+            ('F', 'E'),
+        ]
+        self.assertEqual(build_graph(pairs), {
+            'C': ['A', 'F'],
+            'A': ['B', 'D'],
+            'B': ['E'],
+            'D': ['E'],
+            'F': ['E'],
+        })
+
+    def test_alphabet(self):
+        graph = {
+            'C': ['A', 'F'],
+            'A': ['B', 'D'],
+            'B': ['E'],
+            'D': ['E'],
+            'F': ['E'],
+        }
+        self.assertEqual(alphabet(graph), set(list('ABCDEF')))
+
+    def test_find_no_deps(self):
+        graph = {
+            'C': ['A', 'F'],
+            'A': ['B', 'D'],
+            'B': ['E'],
+            'D': ['E'],
+            'F': ['E'],
+        }
+        self.assertEqual(find_no_deps(graph), set(['C']))
+
+    def test_linearize(self):
+        graph = {
+            'C': ['A', 'F'],
+            'A': ['B', 'D'],
+            'B': ['E'],
+            'D': ['E'],
+            'F': ['E'],
+        }
+        self.assertEqual(linearize(graph), list('CABDFE'))
