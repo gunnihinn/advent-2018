12646989105da0acd69d6bc2558e57dbad756a7f
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2018-12-11 10:32:18 +0100

    Solve part A of day 11

--- py/day11.py
+++ py/day11.py
@@ -1,5 +1,7 @@
+import itertools
+
 def run_a(filename):
-    pass
+    print(max_square(9798))
 
 def run_b(filename):
     pass
@@ -13,3 +15,37 @@ def power_level(x, y, serial):
     level = level % 10
 
     return level - 5
+
+def power(serial):
+    """Return a function that computes the power of an (x,y) coordinate for the
+    given serial number."""
+    cache = {}
+
+    def _p(x, y):
+        if (x,y) not in cache:
+            cache[(x,y)] = power_level(x, y, serial)
+
+        return cache[(x,y)]
+
+    return _p
+
+def square(power, x, y):
+    """Compute the power of a 3x3 square whose top-left coordinate is (x,y)."""
+    return sum((
+        power(_x, _y)
+        for _x in range(x, x+3)
+        for _y in range(y, y+3)
+    ))
+
+def max_square(serial):
+    p = power(serial)
+
+    (maxX, maxY) = (0, 0)
+    maxPower = 0
+
+    for (x, y) in itertools.product(range(1, 298), range(1, 298)):
+        if square(p, x, y) > maxPower:
+            (maxX, maxY) = (x, y)
+            maxPower = square(p, x, y)
+
+    return (maxX, maxY)
--- py/tests/day11_test.py
+++ py/tests/day11_test.py
@@ -9,3 +9,17 @@ class TestExample(unittest.TestCase):
         self.assertEqual(power_level(122, 79, 57), -5)
         self.assertEqual(power_level(217, 196, 39), 0)
         self.assertEqual(power_level(101, 153, 71), 4)
+
+    def test_square1(self):
+        p = power(18)
+        self.assertEqual(square(p, 33, 45), 29)
+
+    def test_square2(self):
+        p = power(42)
+        self.assertEqual(square(p, 21, 61), 30)
+
+    def test_max_square1(self):
+        self.assertEqual(max_square(18), (33, 45))
+
+    def test_max_square2(self):
+        self.assertEqual(max_square(42), (21, 61))
